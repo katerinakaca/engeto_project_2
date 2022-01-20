8ee59145f2601e3c19afc53bc436a94ffd7934c5
katerinakaca <urbancova.kata@seznam.cz>
2022-01-20 16:53:27 +0100

    úvod

---
+++ bulls_and_cows.py
@@ -0,0 +1,12 @@
+
+
+
+print("Welcome in our game Bulls and Cows!")
+oddelovac = "-" * 47
+print(oddelovac)
+print("""
+I've generated a random 4 digit number for you.
+Let's play a bulls and cows game.
+"""
+      )
+print(oddelovac)

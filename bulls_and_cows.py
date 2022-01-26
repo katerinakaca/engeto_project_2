26d5636671c716e055febd33654164669aca8439
katerinakaca <urbancova.kata@seznam.cz>
2022-01-26 14:26:24 +0100

    generace nahodneho cisla kontrola vstupu uzivatele hra + vyhodnoceni

--- bulls_and_cows.py
+++ bulls_and_cows.py
@@ -1,14 +1,14 @@
 import random
 
-# print("Welcome in our game Bulls and Cows!")
-# oddelovac = "-" * 47
-# print(oddelovac)
-# print("""
-# I've generated a random 4 digit number for you.
-# Let's play a bulls and cows game.
-# """
-#       )
-# print(oddelovac)
+print("Welcome in our game Bulls and Cows!")
+oddelovac = "-" * 47
+print(oddelovac)
+print("""
+I've generated a random 4 digit number for you.
+Let's play a bulls and cows game.
+"""
+      )
+print(oddelovac)
 
 #vygenerovat náhodné číslo
 def nahodne_cislo():
@@ -16,6 +16,7 @@ def nahodne_cislo():
       while vybiram_cislo:
             random.seed(1)
             cislo = random.sample(range(10), k = 4)
+            print(cislo)
             if cislo[0] != 0:
                   spravne_cislo = cislo
                   break
@@ -24,54 +25,75 @@ def nahodne_cislo():
       return spravne_cislo
 
 #kontrola vstupu uživatele
-def kontrola_vstupu(hadani):
-      vyber = True
-      while vyber:
+def kontrola_vstupu():
+
+      while True:
             list_cisel = []
+            hadani = input("Enter a number: ")
             for n in hadani:
                   if not n.isnumeric():
                         print("Obsahuje špatné znaky.")
-                        break
+                        continue
                   elif hadani.count(n) > 1:
                         print("Číslice se nesmí opakovat.")
-                        break
+                        continue
                   else:
-                        list_cisel.append(n)
+                        list_cisel.append(int(n))
             if list_cisel[0] == "0" or len(list_cisel) != 4:
                   print("Číslo nesmí začínat 0 a číslo musí být 4místné.")
-                  break
+                  continue
             else:
-                  vyber = False
+                  break
       return list_cisel
 
 #hra samotná
 def hra():
-      hra_bezi = True
-      while hra_bezi == True:
+      pokusy = 0
+      oddelovac = "-" * 47
+      while True:
             cislo = nahodne_cislo()
-            hadani = input("Zadej 4místné číslo: ")
-            spravne_hadani = kontrola_vstupu(hadani)
-            break
-      return spravne_hadani
+            spravne_hadani = kontrola_vstupu()
+            pocet = pocet_bulls_and_cows(cislo, spravne_hadani)
+            pocet_byku = pocet[0]
+            pocet_krav = pocet[1]
+            tisk_poctu_bulls_and_cows(pocet_byku, pocet_krav)
+            print(oddelovac)
+            pokusy += 1
+            if pocet_byku == 4:
+                  break
+      vysledek = []
+      for c in cislo:
+            vysledek.append(str(c))
 
-def porovnani_vstupu(cislo, spravne_hadani):
+      return print(f"Číslo je {''.join(vysledek)}.\n Pro uhodnutí správného výsledku jsi potřeboval {pokusy} pokusů. ")
+
+
+
+def pocet_bulls_and_cows(cislo, spravne_hadani):
       indexace_cisla = list(enumerate(cislo))
+      pocet_byku = 0
+      pocet_krav = 0
       for i, cislice in enumerate(spravne_hadani):
             dvojice = tuple([i, cislice])
-            print(dvojice)
+            if dvojice in indexace_cisla:
+                  pocet_byku +=1
+            elif cislice in cislo and cislo[i] != cislice:
+                  pocet_krav += 1
+      return [pocet_byku, pocet_krav]
+
+def tisk_poctu_bulls_and_cows(pocet_byku, pocet_krav):
+      if pocet_byku == 1 and pocet_krav == 1:
+            return print(f"{pocet_byku} bull,{pocet_krav} cow")
+      elif pocet_byku == 1 and pocet_krav != 1:
+            return print(f"{pocet_byku} bull,{pocet_krav} cows")
+      elif pocet_byku != 1 and pocet_krav == 1:
+            return print(f"{pocet_byku} bulls,{pocet_krav} cow")
+      elif pocet_byku == 4:
+            return print("ANO, uhodls!")
+      else:
+            return print(f"{pocet_byku} bulls,{pocet_krav} cows")
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-print(porovnani_vstupu(["1", "2", "3", "4"], ["1", "2", "3", "4"]))
+hra()
 

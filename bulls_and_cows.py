27c0f9e74c23fdce8fef9430af96d640dbf7087b
katerinakaca <urbancova.kata@seznam.cz>
2022-01-26 14:45:36 +0100

    generace nahodneho cisla kontrola vstupu uzivatele hra + vyhodnoceni TODO - opakovani cisel

--- bulls_and_cows.py
+++ bulls_and_cows.py
@@ -1,7 +1,7 @@
 import random
-
-print("Welcome in our game Bulls and Cows!")
 oddelovac = "-" * 47
+print(oddelovac)
+print("Welcome in our game Bulls and Cows!")
 print(oddelovac)
 print("""
 I've generated a random 4 digit number for you.
@@ -14,7 +14,7 @@ print(oddelovac)
 def nahodne_cislo():
       vybiram_cislo = True
       while vybiram_cislo:
-            random.seed(1)
+            #random.seed(1)
             cislo = random.sample(range(10), k = 4)
             print(cislo)
             if cislo[0] != 0:
@@ -32,15 +32,15 @@ def kontrola_vstupu():
             hadani = input("Enter a number: ")
             for n in hadani:
                   if not n.isnumeric():
-                        print("Obsahuje špatné znaky.")
+                        print("Wrong format.")
                         continue
                   elif hadani.count(n) > 1:
-                        print("Číslice se nesmí opakovat.")
+                        print("The digits must not be repeated.")
                         continue
                   else:
                         list_cisel.append(int(n))
-            if list_cisel[0] == "0" or len(list_cisel) != 4:
-                  print("Číslo nesmí začínat 0 a číslo musí být 4místné.")
+            if list_cisel[0] == 0 or len(list_cisel) != 4:
+                  print("The number must not start with 0 and the number must be four digits.")
                   continue
             else:
                   break
@@ -65,7 +65,7 @@ def hra():
       for c in cislo:
             vysledek.append(str(c))
 
-      return print(f"Číslo je {''.join(vysledek)}.\n Pro uhodnutí správného výsledku jsi potřeboval {pokusy} pokusů. ")
+      return print(f"Correct number is {''.join(vysledek)}.\nYou needed {pokusy} attempts to guess the right result.")
 
 
 
@@ -89,7 +89,7 @@ def tisk_poctu_bulls_and_cows(pocet_byku, pocet_krav):
       elif pocet_byku != 1 and pocet_krav == 1:
             return print(f"{pocet_byku} bulls,{pocet_krav} cow")
       elif pocet_byku == 4:
-            return print("ANO, uhodls!")
+            return print("YES! You got it!")
       else:
             return print(f"{pocet_byku} bulls,{pocet_krav} cows")
 

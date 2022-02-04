b22f6e176f0dd01abb7c3db923de5d3788cfba3a
katerinakaca <urbancova.kata@seznam.cz>
2022-02-04 21:14:52 +0100

    generace nahodneho cisla kontrola vstupu uzivatele hra + vyhodnoceni čas hry

--- bulls_and_cows.py
+++ bulls_and_cows.py
@@ -1,4 +1,7 @@
 import random
+import time
+from datetime import timedelta
+
 oddelovac = "-" * 47
 print(oddelovac)
 print("Welcome in our game Bulls and Cows!")
@@ -10,13 +13,45 @@ Let's play a bulls and cows game.
       )
 print(oddelovac)
 
+#hra samotná
+def hra():
+      hrac = input("NickName player: ")
+      cas_zac = time.time()
+      pokusy = 0
+      oddelovac = "-" * 47
+      cislo = nahodne_cislo()
+      while True:
+            spravne_hadani = kontrola_vstupu()
+            pocet = pocet_bulls_and_cows(cislo, spravne_hadani)
+            pocet_byku = pocet[0]
+            pocet_krav = pocet[1]
+            tisk_poctu_bulls_and_cows(pocet_byku, pocet_krav)
+            print(oddelovac)
+            pokusy += 1
+            if pocet_byku == 4:
+                  break
+      vysledek = []
+      for c in cislo:
+            vysledek.append(str(c))
+
+      vysledny_cas = int((time.time() - cas_zac))
+      # print(str(timedelta(seconds=vysledny_cas)))
+
+      return print(
+            f"""
+Correct number is {''.join(vysledek)}.
+You needed {pokusy} attempts to guess the right result.
+Time of your game: {str(timedelta(seconds=vysledny_cas))}
+            """)
+
+
 #vygenerovat náhodné číslo
 def nahodne_cislo():
       vybiram_cislo = True
       while vybiram_cislo:
             #random.seed(1)
             cislo = random.sample(range(10), k = 4)
-            print(cislo)
+            # print(cislo)
             if cislo[0] != 0:
                   spravne_cislo = cislo
                   break
@@ -36,39 +71,18 @@ def kontrola_vstupu():
                         continue
                   elif hadani.count(n) > 1:
                         print("The digits must not be repeated.")
-                        continue
+                        break
                   else:
                         list_cisel.append(int(n))
-            if list_cisel[0] == 0 or len(list_cisel) != 4:
+            if list_cisel and list_cisel[0] == 0 or len(list_cisel) != 4:
                   print("The number must not start with 0 and the number must be four digits.")
                   continue
             else:
                   break
       return list_cisel
 
-#hra samotná
-def hra():
-      pokusy = 0
-      oddelovac = "-" * 47
-      while True:
-            cislo = nahodne_cislo()
-            spravne_hadani = kontrola_vstupu()
-            pocet = pocet_bulls_and_cows(cislo, spravne_hadani)
-            pocet_byku = pocet[0]
-            pocet_krav = pocet[1]
-            tisk_poctu_bulls_and_cows(pocet_byku, pocet_krav)
-            print(oddelovac)
-            pokusy += 1
-            if pocet_byku == 4:
-                  break
-      vysledek = []
-      for c in cislo:
-            vysledek.append(str(c))
 
-      return print(f"Correct number is {''.join(vysledek)}.\nYou needed {pokusy} attempts to guess the right result.")
-
-
-
+#počet býků a krav
 def pocet_bulls_and_cows(cislo, spravne_hadani):
       indexace_cisla = list(enumerate(cislo))
       pocet_byku = 0
@@ -81,6 +95,7 @@ def pocet_bulls_and_cows(cislo, spravne_hadani):
                   pocet_krav += 1
       return [pocet_byku, pocet_krav]
 
+#vytisknutí
 def tisk_poctu_bulls_and_cows(pocet_byku, pocet_krav):
       if pocet_byku == 1 and pocet_krav == 1:
             return print(f"{pocet_byku} bull,{pocet_krav} cow")
